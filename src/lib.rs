d001f88828b21330993daaecef3c64c9c50b28e9
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-29 14:50:07 -0700

    initial struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
-// unimplemented!()
+pub struct Runner {}
+
+impl Runner {
+    pub async fn init() -> Runner {
+        Runner {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init() {
+        Runner::init().await;
+    }
+}
