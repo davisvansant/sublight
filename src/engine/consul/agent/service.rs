7af5cc2533b9574e83738e48e767dd31970de945
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-05-20 10:50:13 -0700

    initial agent service endpoints, tests

--- src/engine/consul/agent/service.rs
+++ src/engine/consul/agent/service.rs
@@ -1,11 +1,8 @@
 use crate::{Body, Error, Method, Response, Runner};
 
-pub mod checks;
-pub mod service;
-
 impl Runner {
-    pub async fn list_members(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/members";
+    pub async fn agent_services(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/agent/services";
         let uri = self.build_uri(path).await;
         let method = Method::GET;
         let body = Body::empty();
@@ -14,9 +11,12 @@ impl Runner {
         Ok(response)
     }
 
-    pub async fn agent_self(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/self";
-        let uri = self.build_uri(path).await;
+    pub async fn agent_service_configuration(
+        &self,
+        service_id: &str,
+    ) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/agent/service/{}", service_id);
+        let uri = self.build_uri(&path).await;
         let method = Method::GET;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
@@ -24,8 +24,31 @@ impl Runner {
         Ok(response)
     }
 
-    pub async fn agent_reload(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/reload";
+    pub async fn agent_health_service_name(
+        &self,
+        service_name: &str,
+    ) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/agent/health/service/name/{}", service_name);
+        let uri = self.build_uri(&path).await;
+        let method = Method::GET;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn agent_health_service_id(&self, service_id: &str) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/agent/health/service/id/{}", service_id);
+        let uri = self.build_uri(&path).await;
+        let method = Method::GET;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn agent_service_register(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/agent/service/register";
         let uri = self.build_uri(path).await;
         let method = Method::PUT;
         let body = Body::empty();
@@ -34,38 +57,11 @@ impl Runner {
         Ok(response)
     }
 
-    pub async fn agent_maintenance(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/maintentance";
-        let uri = self.build_uri(path).await;
-        let method = Method::PUT;
-        let body = Body::empty();
-        let request = self.build_request(method, uri, body).await;
-        let response = self.client.request(request).await?;
-        Ok(response)
-    }
-
-    pub async fn agent_metrics(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/metrics";
-        let uri = self.build_uri(path).await;
-        let method = Method::GET;
-        let body = Body::empty();
-        let request = self.build_request(method, uri, body).await;
-        let response = self.client.request(request).await?;
-        Ok(response)
-    }
-
-    pub async fn agent_monitor(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/monitor";
-        let uri = self.build_uri(path).await;
-        let method = Method::GET;
-        let body = Body::empty();
-        let request = self.build_request(method, uri, body).await;
-        let response = self.client.request(request).await?;
-        Ok(response)
-    }
-
-    pub async fn agent_join(&self, address: &str) -> Result<Response<Body>, Error> {
-        let path = format!("/v1/agent/join/{}", address);
+    pub async fn agent_service_deregister(
+        &self,
+        service_id: &str,
+    ) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/agent/service/deregister/{}", service_id);
         let uri = self.build_uri(&path).await;
         let method = Method::PUT;
         let body = Body::empty();
@@ -74,9 +70,12 @@ impl Runner {
         Ok(response)
     }
 
-    pub async fn agent_leave(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/agent/leave";
-        let uri = self.build_uri(path).await;
+    pub async fn agent_service_maintenance(
+        &self,
+        service_id: &str,
+    ) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/agent/service/maintentance/{}", service_id);
+        let uri = self.build_uri(&path).await;
         let method = Method::PUT;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
@@ -91,120 +90,115 @@ mod tests {
     use mockito::mock;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn list_members() -> Result<(), Error> {
+    async fn agent_services() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("GET", "/v1/agent/members")
+        let mock = mock("GET", "/v1/agent/services")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.list_members().await?;
+        test_runner.agent_services().await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_self() -> Result<(), Error> {
+    async fn agent_service_configuration() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("GET", "/v1/agent/self")
+        let mock = mock("GET", "/v1/agent/service/test_service_id")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_self().await?;
+        test_runner
+            .agent_service_configuration("test_service_id")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_reload() -> Result<(), Error> {
+    async fn agent_health_service_name() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("PUT", "/v1/agent/reload")
+        let mock = mock("GET", "/v1/agent/health/service/name/test_service_name")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_reload().await?;
+        test_runner
+            .agent_health_service_name("test_service_name")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_maintenance() -> Result<(), Error> {
+    async fn agent_health_service_id() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("PUT", "/v1/agent/maintentance")
+        let mock = mock("GET", "/v1/agent/health/service/id/test_service_id")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_maintenance().await?;
+        test_runner
+            .agent_health_service_id("test_service_id")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_metrics() -> Result<(), Error> {
+    async fn agent_service_register() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("GET", "/v1/agent/metrics")
+        let mock = mock("PUT", "/v1/agent/service/register")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_metrics().await?;
+        test_runner.agent_service_register().await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_monitor() -> Result<(), Error> {
+    async fn agent_service_deregister() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("GET", "/v1/agent/monitor")
+        let mock = mock("PUT", "/v1/agent/service/deregister/test_service_id")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_monitor().await?;
+        test_runner
+            .agent_service_deregister("test_service_id")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn agent_join() -> Result<(), Error> {
+    async fn agent_service_maintenance() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("PUT", "/v1/agent/join/1.2.3.4")
+        let mock = mock("PUT", "/v1/agent/service/maintentance/test_service_id")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.agent_join("1.2.3.4").await?;
-        mock.assert();
-        assert!(mock.matched());
-        Ok(())
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn agent_leave() -> Result<(), Error> {
-        let test_mock_url = mockito::server_url();
-        let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("PUT", "/v1/agent/leave")
-            .with_status(200)
-            .with_header("user-agent", "sublight/0.1.0")
-            .with_body("")
-            .create();
-        test_runner.agent_leave().await?;
+        test_runner
+            .agent_service_maintenance("test_service_id")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
