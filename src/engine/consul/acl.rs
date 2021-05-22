6dfbae6cc589a23b903bc68151e10ab22f943a75
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-05-22 15:02:45 -0700

    update acl endpoints, tests

--- src/engine/consul/acl.rs
+++ src/engine/consul/acl.rs
@@ -1,9 +1,39 @@
 use crate::{Body, Error, Method, Response, Runner};
 
 impl Runner {
-    pub async fn bootstrap(&self) -> Result<Response<Body>, Error> {
+    pub async fn acl_bootstrap(&self) -> Result<Response<Body>, Error> {
         let path = "/v1/acl/bootstrap";
         let method = Method::PUT;
+        let uri = self.build_uri(path).await;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn acl_replication(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/acl/replication";
+        let method = Method::GET;
+        let uri = self.build_uri(path).await;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn acl_login(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/acl/login";
+        let method = Method::POST;
+        let uri = self.build_uri(path).await;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn acl_logout(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/acl/logout";
+        let method = Method::POST;
         let uri = self.build_uri(path).await;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
@@ -18,7 +48,7 @@ mod tests {
     use mockito::mock;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn bootstrap() -> Result<(), Error> {
+    async fn acl_bootstrap() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
         let mock = mock("PUT", "/v1/acl/bootstrap")
@@ -26,7 +56,52 @@ mod tests {
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.bootstrap().await?;
+        test_runner.acl_bootstrap().await?;
+        mock.assert();
+        assert!(mock.matched());
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn acl_replication() -> Result<(), Error> {
+        let test_mock_url = mockito::server_url();
+        let test_runner = Runner::init(&test_mock_url, None, None).await;
+        let mock = mock("GET", "/v1/acl/replication")
+            .with_status(200)
+            .with_header("user-agent", "sublight/0.1.0")
+            .with_body("")
+            .create();
+        test_runner.acl_replication().await?;
+        mock.assert();
+        assert!(mock.matched());
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn acl_login() -> Result<(), Error> {
+        let test_mock_url = mockito::server_url();
+        let test_runner = Runner::init(&test_mock_url, None, None).await;
+        let mock = mock("POST", "/v1/acl/login")
+            .with_status(200)
+            .with_header("user-agent", "sublight/0.1.0")
+            .with_body("")
+            .create();
+        test_runner.acl_login().await?;
+        mock.assert();
+        assert!(mock.matched());
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn acl_logout() -> Result<(), Error> {
+        let test_mock_url = mockito::server_url();
+        let test_runner = Runner::init(&test_mock_url, None, None).await;
+        let mock = mock("POST", "/v1/acl/logout")
+            .with_status(200)
+            .with_header("user-agent", "sublight/0.1.0")
+            .with_body("")
+            .create();
+        test_runner.acl_logout().await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
