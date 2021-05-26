01a97a82e71b0476e04499f08b16887c0c003cb1
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-05-26 11:13:09 -0700

    initial acl auth method endpoints, tests

--- src/engine/consul/acl/auth_method.rs
+++ src/engine/consul/acl/auth_method.rs
@@ -1,13 +1,8 @@
 use crate::{Body, Error, Method, Response, Runner};
 
-pub mod auth_method;
-pub mod policy;
-pub mod role;
-pub mod token;
-
 impl Runner {
-    pub async fn acl_bootstrap(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/acl/bootstrap";
+    pub async fn acl_auth_method_create(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/acl/auth-method";
         let method = Method::PUT;
         let uri = self.build_uri(path).await;
         let body = Body::empty();
@@ -16,29 +11,39 @@ impl Runner {
         Ok(response)
     }
 
-    pub async fn acl_replication(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/acl/replication";
+    pub async fn acl_auth_method_read(&self, name: &str) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/acl/auth-method/{}", name);
         let method = Method::GET;
-        let uri = self.build_uri(path).await;
+        let uri = self.build_uri(&path).await;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
         let response = self.client.request(request).await?;
         Ok(response)
     }
 
-    pub async fn acl_login(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/acl/login";
-        let method = Method::POST;
-        let uri = self.build_uri(path).await;
+    pub async fn acl_auth_method_update(&self, name: &str) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/acl/auth-method/{}", name);
+        let method = Method::PUT;
+        let uri = self.build_uri(&path).await;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
         let response = self.client.request(request).await?;
         Ok(response)
     }
 
-    pub async fn acl_logout(&self) -> Result<Response<Body>, Error> {
-        let path = "/v1/acl/logout";
-        let method = Method::POST;
+    pub async fn acl_auth_method_delete(&self, name: &str) -> Result<Response<Body>, Error> {
+        let path = format!("/v1/acl/auth-method/{}", name);
+        let method = Method::DELETE;
+        let uri = self.build_uri(&path).await;
+        let body = Body::empty();
+        let request = self.build_request(method, uri, body).await;
+        let response = self.client.request(request).await?;
+        Ok(response)
+    }
+
+    pub async fn acl_auth_methods(&self) -> Result<Response<Body>, Error> {
+        let path = "/v1/acl/auth-methods";
+        let method = Method::GET;
         let uri = self.build_uri(path).await;
         let body = Body::empty();
         let request = self.build_request(method, uri, body).await;
@@ -53,60 +58,81 @@ mod tests {
     use mockito::mock;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn acl_bootstrap() -> Result<(), Error> {
+    async fn acl_auth_method_create() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("PUT", "/v1/acl/bootstrap")
+        let mock = mock("PUT", "/v1/acl/auth-method")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.acl_bootstrap().await?;
+        test_runner.acl_auth_method_create().await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn acl_replication() -> Result<(), Error> {
+    async fn acl_auth_method_read() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("GET", "/v1/acl/replication")
+        let mock = mock("GET", "/v1/acl/auth-method/test_auth_method_name")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.acl_replication().await?;
+        test_runner
+            .acl_auth_method_read("test_auth_method_name")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn acl_login() -> Result<(), Error> {
+    async fn acl_auth_method_update() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("POST", "/v1/acl/login")
+        let mock = mock("PUT", "/v1/acl/auth-method/test_auth_method_name")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.acl_login().await?;
+        test_runner
+            .acl_auth_method_update("test_auth_method_name")
+            .await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn acl_logout() -> Result<(), Error> {
+    async fn acl_auth_method_delete() -> Result<(), Error> {
         let test_mock_url = mockito::server_url();
         let test_runner = Runner::init(&test_mock_url, None, None).await;
-        let mock = mock("POST", "/v1/acl/logout")
+        let mock = mock("DELETE", "/v1/acl/auth-method/test_auth_method_name")
             .with_status(200)
             .with_header("user-agent", "sublight/0.1.0")
             .with_body("")
             .create();
-        test_runner.acl_logout().await?;
+        test_runner
+            .acl_auth_method_delete("test_auth_method_name")
+            .await?;
+        mock.assert();
+        assert!(mock.matched());
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn acl_auth_methods() -> Result<(), Error> {
+        let test_mock_url = mockito::server_url();
+        let test_runner = Runner::init(&test_mock_url, None, None).await;
+        let mock = mock("GET", "/v1/acl/auth-methods")
+            .with_status(200)
+            .with_header("user-agent", "sublight/0.1.0")
+            .with_body("")
+            .create();
+        test_runner.acl_auth_methods().await?;
         mock.assert();
         assert!(mock.matched());
         Ok(())
